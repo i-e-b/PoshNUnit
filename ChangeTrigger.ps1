c4cac091e773479e69a15d954cc5dbee6ebb684a
Iain Ballard <ieb@me.com>
2011-12-07 08:35:26 +0000

    nunit path

--- ChangeTrigger.ps1
+++ ChangeTrigger.ps1
@@ -1,7 +1,5 @@
 param($changedFile = $(throw "change triggering file must be provided"))
 
-$nunit = "C:\Program Files\NUnit 2.5.10\bin\net-2.0\nunit-console-x86.exe"
-
 $workingDirectory = .\SolutionBuilder.ps1 $changedFile
 .\NUnitRunner.ps1 $workingDirectory
 
--- NUnitRunner.ps1
+++ NUnitRunner.ps1
@@ -2,6 +2,7 @@ param($buildDirectory = $(throw "build results directory must be provided"))
 # Test against all the dlls we can find, report any failures.
 #todo: configurable pattern for test assemblies?
 
+$nunit = "${env:ProgramFiles(x86)}\NUnit 2.5.10\bin\net-2.0\nunit-console-x86.exe"
 $results = "$buildDirectory\_PoshNUnit_Results.xml"
 $dlls = ""
 

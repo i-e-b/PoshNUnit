a06326ed94fe45230feae446785ce75176d732f5
Iain Ballard <ieb@me.com>
2012-11-13 22:40:44 +0000

    .net 4, other refinements

--- NUnitRunner.ps1
+++ NUnitRunner.ps1
@@ -1,14 +1,14 @@
 param(
 	$buildDirectory = $(throw "build results directory must be provided"),
-	$testAssmPattern = "*.dll" # replace with *.dll to test everything -- this is quite slow!
+	$testAssmPattern = "*test*.dll" # replace with *.dll to test everything -- this is quite slow!
 )
 # Test against all the dlls we can find, report any failures.
 #todo: configurable pattern for test assemblies?
 
 $PossibleNunits = @(
-	"${env:ProgramFiles(x86)}\NUnit*\bin\nunit-console.exe",
-	"${env:ProgramFiles}\NUnit*\bin\net-4.0\nunit-console-x86.exe",
-	"${env:ProgramFiles(x86)}\NUnit*\bin\net-4.0\nunit-console-x86.exe");
+	"${env:ProgramFiles}\NUnit*\bin\net-4.0\nunit-console.exe",
+	"${env:ProgramFiles(x86)}\NUnit*\bin\net-4.0\nunit-console.exe",
+	"${env:ProgramFiles(x86)}\NUnit*\bin\nunit-console.exe");
 
 $nunit = $PossibleNunits | %{ ls $_ } | ?{ Test-Path $_ } | select-object -first 1
 if (-not (Test-Path $nunit)) {
@@ -22,7 +22,7 @@ $dlls = ls -Filter $testAssmPattern -Path $buildDirectory
 if (Test-Path $results) { rm $results }
 
 pushd $buildDirectory
-& $nunit $dlls /xml=$results | out-null
+& $nunit $dlls /framework=v4.0 /xml=$results | out-null
 popd
 
 if (-not (Test-Path $results)) {
@@ -37,15 +37,19 @@ $passed = 0
 $failed = 0
 $other = 0
 $tests | %{
-	if ($_.success -eq "True") {$passed++}
-	elseif ($_.success -eq "False") {$failed++}
-	else {$other++}
+	if ($_.result -eq "success") {$passed++}
+	elseif ($_.result -eq "inconclusive") {$other++}
+    elseif ($_.result -eq "Ignored") {$other++}
+	else {$failed++}
 }
 Write-Host "$passed tests passed " -fo green -NoNewline
 if ($other -gt 0) {Write-Host "$other inconclusive or ignored " -fo yellow -NoNewline}
 if ($failed -gt 0) {Write-Host "$failed failed " -fo red -NoNewLine}
 Write-Host ";"
-$tests | %{ if ($_.success -eq "False") {
+if ($failed -eq 0) {$tests | %{ if (($_.result -eq "ignored") -or ($_.result -eq "inconclusive")) {
+	Write-Host $_.name -fo yellow
+}}}
+$tests | %{ if ($_.result -eq "Failure") {
 	Write-Host $_.name -fo red
 }}
 
